17e60312789c80996b49d1fd749408c2449df512
Erciyuan11 <891054582@qq.com>
2025-02-24 21:53:57 +0800

    上传主角基类

--- Assets/Scripts/CharactorBasic.cs
+++ Assets/Scripts/CharactorBasic.cs
@@ -1,22 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 
 public class CharactorBasic : MonoBehaviour
 {
-    public float maxhp = 1000;
-    float currenthp;
-    public float speed = 20;
-    public float shield = 0;
-    public float attack = 100;
-    //0为普攻 1为小技能 2为大招
-    // Start is called before the first frame update
-    void Start()
+    public string charactername;
+    public float maxhp;
+    public float currenthp;
+    public float speed;
+    public float maxmagic;//战技点数
+    public float currentmagic;
+    public float maxultskill;//大招
+    public float currentskill;
+    public float shield;
+    public float attack;
+
+    protected virtual void Awake()
     {
-        
+
     }
+    protected virtual void Start()
+    {
+
+    }
+    // Start is called before the first frame update
+
 
     // Update is called once per frame
     void Update()
